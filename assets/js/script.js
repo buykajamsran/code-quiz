5da36be490d43946c99f41586f9aaa1ba9b4dfd9
buykajamsran <buyka.jamsran@gmail.com>
2021-12-19 19:49:39 -0600

    quiz questions, score info and form for high scores added

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+var header = document.getElementById("header");
+var intro = document.getElementById("intro");
+var quiz = document.getElementById("quiz");
+
+var question = document.getElementById("question");
+var choices = document.getElementById("choices");
+
+var choice1 = document.getElementById("1");
+var choice2 = document.getElementById("2");
+var choice3 = document.getElementById("3");
+var choice4= document.getElementById("4");
+
+var finalScore= document.getElementById("finalScore");
+var endMessage = document.getElementById("endMessage");
+var result = document.getElementById("result");
+var scoreList = document.getElementById("scorelist");
+
+var questions = [
+    { question: 'Commonly used data types DO NOT include:', 
+    choice1 : "1. strings",
+    choice2 : "2. booleans",
+    choice3 : "3. alerts",
+    choice4 : "4. numbers",
+    correct: "3"
+    },
+    { question: "The condition in an if / else statement is enclosed with ________.", 
+    choice1 : "1. quotes",
+    choice2 : "2. curly brackets",
+    choice3 : "3. parenthesis",
+    choice4 : "4. square brackets",
+    correct: "2"
+    },
+    { question: "Arrays in JavaScript can be used to store ________.", 
+    choice1 : "1. numbers and strings",
+    choice2 : "2. other arrays",
+    choice3 : "3. booleans",
+    choice4 : "4. all of the above",
+    correct: "4"
+    },
+    { question: "String values must be enclosed within ______ when being assigned to variables.", 
+    choice1 : "1. commas",
+    choice2 : "2. curly brackets",
+    choice3 : "3. quotes",
+    choice4 : "4. parenthesis",
+    correct: "3"
+    },
+    { question: "A very useful tool used during development and debugging for printing content to the debugger is:", 
+    choice1 : "1. JavaScript",
+    choice2 : "2. terminal/bash",
+    choice3 : "3. for loops",
+    choice4 : "4. console.log",
+    correct: "4"
+    },
+]  
+
